90190e5648e157508206b1287d27d2191a24c73b
Kazuhiro SERA <seratch@gmail.com>
2011-01-25 01:09:21 +0900

    Added Chapter 2.3.2

---
+++ python/chapter02/2.3.1_main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+from math import sqrt
+
+# Euclidean distance
+euclidean_distance = sqrt(pow(5-4,2)+pow(4-1,2))
+print euclidean_distance
+
+# avoid zero divide
+inversed_ed = 1/(1+sqrt(pow(5-4,2)+pow(4-1,2)))
+print inversed_ed
+
+import recommendations
+sim_distance_result = recommendations.sim_distance(
+  recommendations.critics, 
+  'Lisa Rose', 
+  'Gene Seymour'
+)
+print sim_distance_result
+
+sim_pearson_result = recommendations.sim_pearson(
+  recommendations.critics, 
+  'Lisa Rose', 
+  'Gene Seymour'
+)
+print sim_pearson_result
+
--- python/chapter02/recommendations.py
+++ python/chapter02/recommendations.py
@@ -10,5 +10,54 @@ critics = {
     'Toby'             : {                         'Snakes on a Plane':4.5,                     'Superman Returns':4.0, 'You, Me and Dupree':1.0,                         },
 }
 
-#print critics
+from math import sqrt
+
+# ---
+# Chapter 2.3.1 Euclidean distance
+# ---
+def sim_distance(prefs,person1,person2):
+  # get items judged by both of person1 and person2
+  si = {}
+  for item in prefs[person1]:
+    if item in prefs[person2]:
+      si[item] = 1
+  # not found
+  if len(si) == 0: 
+    return 0
+  # sum all square root of items
+  sum_of_squares = sum(
+    [ 
+      pow(prefs[person1][item]-prefs[person2][item],2) 
+        for item in prefs[person1] if item in prefs[person2] 
+    ]
+  )
+  return 1/(1+sum_of_squares)
+
+# ---
+# Chapter 2.3.2 Pearson correlation coefficient
+# ---
+def sim_pearson(prefs,p1,p2):
+  # get items judged by both
+  si = {}
+  for item in prefs[p1]:
+    if item in prefs[p2]: 
+      si[item] = 1
+  n = len(si)
+  # not found
+  if n == 0:
+    return 0
+  # sum all
+  sum1 = sum([prefs[p1][it] for it in si])
+  sum2 = sum([prefs[p2][it] for it in si])
+  # sum all square root
+  sum1sq = sum([pow(prefs[p1][it],2) for it in si])
+  sum2sq = sum([pow(prefs[p2][it],2) for it in si])
+  # sum products
+  sum_products = sum([prefs[p1][it] * prefs[p2][it] for it in si])
+  num = sum_products - (sum1*sum2/n)
+  den = sqrt((sum1sq-pow(sum1,2)/n)*(sum2sq-pow(sum2,2)/n))
+  if den == 0:
+    return 0
+  return num/den
+
 
