9c9432a1ca427db11c53123f05b144568aecf144
Kazuhiro SERA <seratch@gmail.com>
2011-02-15 12:53:18 +0900

    Refactored

--- scala/learning-ci-scala/src/main/scala/sandbox/observer/OreillyProgrammingScalaChapter13.scala
+++ scala/learning-ci-scala/src/main/scala/sandbox/observer/OreillyProgrammingScalaChapter13.scala
@@ -3,98 +3,62 @@ package sandbox.observer
 /**
  * http://programming-scala.labs.oreilly.com/ch13.html
  */
-abstract class SubjectObserver {
-
+abstract class AbstractContainer {
   type S <: Subject
   type O <: Observer
 
   trait Subject {
-
     // self-type annotation
     // we can now use "self" as an alias for "this"
     self: S =>
-
     private var observers = List[O]()
 
     def addObserver(observer: O) = observers ::= observer
 
-    //[error] /Users/seratch/IdeaProjects/learning-ci-scala/scala/learning-ci-scala/src/main/scala/sandbox/observer/SubjectObserver.scala:14: type mismatch;
-    //[error]  found   : Subject.this.type (with underlying type SubjectObserver.this.Subject)
-    //[error]  required: SubjectObserver.this.S
+    //[error] /Users/seratch/IdeaProjects/learning-ci-scala/scala/learning-ci-scala/src/main/scala/sandbox/observer/AbstractContainer.scala:14: type mismatch;
+    //[error]  found   : Subject.this.type (with underlying type AbstractContainer.this.Subject)
+    //[error]  required: AbstractContainer.this.S
     //[error]     def notifyObservers = observers foreach (_.receiveUpdate(this))
     def notifyObservers = observers foreach (_.receiveUpdate(self))
-
   }
 
   trait Observer {
-
     def receiveUpdate(subject: S)
-
-  }
-
-}
-
-object ButtonSubjectObserver extends SubjectObserver {
-
-  type S = ObservableButton
-  type O = ButtonObserver
-
-  class ObservableButton(name: String) extends Button(name) with Subject {
-
-    override def click() = {
-      super.click
-      notifyObservers
-    }
-
-  }
-
-  trait ButtonObserver extends Observer {
-
-    def receiveUpdate(button: ObservableButton)
-
-  }
-
-}
-
-import scala.collection.mutable.HashMap
-
-class ButtonClickObserver extends ButtonSubjectObserver.ButtonObserver {
-
-  val clicks = new HashMap[String, Int]
-
-  def receiveUpdate(button: ButtonSubjectObserver.ObservableButton) = {
-    val count = clicks.getOrElse(button.label, 0) + 1
-    clicks.update(button.label, count)
   }
 
 }
 
 trait Clickable {
-
   def click()
-
 }
 
-class Widget {
-
-}
-
-class Button(val label: String) extends Widget with Clickable {
-
+class Button(val label: String) extends Clickable {
   def click() = println("click!")
-
 }
 
-trait VetoableClicks extends Clickable {
+object Container extends AbstractContainer {
+  type S = ObservableButton
+  type O = ButtonObserver
 
-  val maxAllowed = 1
+  class ObservableButton(name: String) extends Button(name) with Subject {
+    override def click() = {
+      super.click
+      notifyObservers
+    }
+  }
 
-  private var count = 0
+  trait ButtonObserver extends Observer {
+    def receiveUpdate(button: ObservableButton)
+  }
 
-  abstract override def click() = {
-    if (count < maxAllowed) {
-      count += 1
-      super.click()
+  import scala.collection.mutable.HashMap
+
+  class ButtonClickObserver extends Container.ButtonObserver {
+    val clicks = new HashMap[String, Int]
+
+    def receiveUpdate(button: Container.ObservableButton) = {
+      val count = clicks.getOrElse(button.label, 0) + 1
+      clicks.update(button.label, count)
     }
   }
 
@@ -102,11 +66,11 @@ trait VetoableClicks extends Clickable {
 
 object Main extends Application {
 
-  val button1 = new ButtonSubjectObserver.ObservableButton("button1")
-  val button2 = new ButtonSubjectObserver.ObservableButton("button2")
-  val button3 = new ButtonSubjectObserver.ObservableButton("button3")
+  val button1 = new Container.ObservableButton("button1")
+  val button2 = new Container.ObservableButton("button2")
+  val button3 = new Container.ObservableButton("button3")
 
-  val observer = new ButtonClickObserver
+  val observer = new Container.ButtonClickObserver
 
   button1.addObserver(observer)
   button2.addObserver(observer)
